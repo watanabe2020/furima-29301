be4b9a5636b8d46eb4d59685b2808c3952f5d67e
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-01 12:02:05 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
+        :recoverable, :rememberable, :validatable
+        has_many :items
+        has_many :buyers
+
 end
+
+
