a6d83551d3c7b73fa84fd8e5d7a71dbaf0e832d5
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-01 14:40:39 +0900

    バリデーションの入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
         has_many :items
         has_many :buyers
 
+
+        with_options presence: true do
+        validates :nickname format: { with: /\A[a-z0-9]+\z/i, message: "半角文字で入力してください"}
+        validates :mail, format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.massage: "もう一度入力してください" }
+        validates :last_name  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
+        validates :first_name  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
+        validates :last_name_kana  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角で入力してください"}
+        validates :first_name_kana  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角で入力してください"}
+        validates :birthday_chack format: { with: /\A\d{4}\d{2}\d{2}\z/ }
+        end
+
 end
-
-
