8a1e318dbec86c6d44907ce97ab5aaaee2175387
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-02 12:20:28 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
 
         with_options presence: true do
-        validates :nickname format: { with: /\A[a-z0-9]+\z/i, message: "半角文字で入力してください"}
-        validates :mail, format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.massage: "もう一度入力してください" }
-        validates :last_name  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
-        validates :first_name  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
-        validates :last_name_kana  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角で入力してください"}
-        validates :first_name_kana  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角で入力してください"}
-        validates :birthday_chack format: { with: /\A\d{4}\d{2}\d{2}\z/ }
+        validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "半角文字で入力してください"}
+        validates :email, format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,massage: "もう一度入力してください" }
+        validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
+        validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください"}
+        validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角で入力してください"}
+        validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角で入力してください"}
+        validates :birthday
         end
 
 end
