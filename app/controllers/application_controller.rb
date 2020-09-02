822404c2a64c10c745d3c9fe965019d805596a5c
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-02 12:46:42 +0900

    ログインしていないとログイン画面に遷移を消す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
-
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  #いらないでしょうか？
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
