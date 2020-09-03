797ce556eb98668cbc9b048c82930e054cb5dab5
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-03 12:20:01 +0900

    バリデーション設計

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_024013) do
+ActiveRecord::Schema.define(version: 2020_09_03_030722) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "product_description", null: false
+    t.integer "category_id", null: false
+    t.integer "product_status_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "ship_from_id", null: false
+    t.integer "date_of_shipment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_01_024013) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
