f3387d62b8359b3c1898d59fa693cfb72efb525a
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-03 11:15:41 +0900

    user.createをuser.newに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,21 @@
 class UsersController < ApplicationController
-
-def new
+  def new
     @user = User.new
-end
+  end
 
-def create
-  @user=User.create(user_params)
- if @user.valid?
-  @user.save
-  return redirect_to root_path
-else
-  render "new"
-end
-end
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render 'new'
+   end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:nickname, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday)
   end
-
 end
-
-
