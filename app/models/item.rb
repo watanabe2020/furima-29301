9d896a434602431887e17aceb6c00d6c3dd6fd72
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-03 15:30:49 +0900

    バリデーション変更１

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,12 +11,12 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name,length: {maximum: 40}
-    validates :price#,{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
     validates :product_description, length: {maximum: 1000}
-    validates :category_id
-    validates :product_status_id
-    validates :ship_from_id
-    validates :date_of_shipment_id
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :product_status_id, numericality: { other_than: 1 } 
+    validates :ship_from_id, numericality: { other_than: 1 } 
+    validates :date_of_shipment_id, numericality: { other_than: 1 } 
   end
 
 end
