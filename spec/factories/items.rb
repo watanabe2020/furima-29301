cfc9cca2fdad505b859c55b66374cace5a137205
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-05 11:25:12 +0900

    itemテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name                      { 'アイテム' }
+    price                     { '10000' }
+    product_description       { '商品商品商品' }
+    category_id               { '3' }
+    product_status_id         { '2' }
+    postage_id                { '2' }
+    ship_from_id              { '2' }
+    date_of_shipment_id       { '2' }
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,20 +2,86 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
 
+  describe '商品新規登録' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('public/images/test_image.png')
+    end
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@item).to be_valid
+    end
 
+    it '商品名が空だと保存できないこと' do
+      @item.name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
 
-  ログインしているユーザーだけが、出品ページへ遷移できること
-  # 画像は1枚必須であること(ActiveStorageを使用すること)
-  # 商品名が必須であること
-  # 商品の説明が必須であること
-  # カテゴリーの情報が必須であること
-  # 商品の状態についての情報が必須であること
-  # 配送料の負担についての情報が必須であること
-  # 発送元の地域についての情報が必須であること
-  # 発送までの日数についての情報が必須であること
-  # 価格についての情報が必須であること
-  # 価格の範囲が、¥300~¥9,999,999の間であること
-  # 販売価格は半角数字のみ入力可能であること
-  # 入力された販売価格によって、非同期的に販売手数料や販売利益が変わること(JavaScriptを使用して実装すること)
-  # エラーハンドリングができていること（適切では無い値が入力された場合、情報は保存されず、エラーメッセージを出力させる）
+    it '商品名説明が空だと保存できないこと' do
+      @item.product_description  = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product description can't be blank")
+    end
+
+    it 'カテゴリーが空だと保存できないこと' do
+      @item.category_id   = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+    end
+
+    it '商品の状態が空だと保存できないこと' do
+      @item.product_status_id   = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Product status can't be blank", "Product status is not a number")
+    end
+
+    it '送料が空だと保存できないこと' do
+      @item.postage_id   = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Postage can't be blank", "Postage is not a number")
+    end
+
+    it '発送元が空だと保存できないこと' do
+      @item.ship_from_id   = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship from can't be blank", "Ship from is not a number")
+    end
+
+    it '発送日が空だと保存できないこと' do
+      @item.date_of_shipment_id   = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Date of shipment can't be blank", "Date of shipment is not a number")
+    end
+
+    it '販売価格が空だと保存できないこと' do
+      @item.price  = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank",
+        "Price is out of setting range",
+        "Price can't be blank",
+        "Price 半角文字で入力してください")
+    end
+
+    it '販売価格が全角だと保存できないこと' do
+      @item.price  = "１０００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+
+    it 'priceが300円未満では保存できないこと' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+    it 'priceが1,000,000円を超過すると保存できないこと' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is out of setting range")
+    end
+
+    # @item.errors.full_messages
+
+    # bundle exec rspec spec/models/item_spec.rb
+  end
 end
+
