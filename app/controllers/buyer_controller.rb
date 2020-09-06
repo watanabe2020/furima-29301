66c3d529743a150669ed7ea63e62ccc9177f2370
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-06 18:49:49 +0900

    出品者はURLを直接入力してもトップぺージに飛ぶ

--- app/controllers/buyer_controller.rb
+++ app/controllers/buyer_controller.rb
@@ -1,5 +1,8 @@
 class BuyerController < ApplicationController
-  before_action :move_to_index, except:
+  before_action :move_to_index
+  before_action :current_user_cant_buy
+
+
 
   def index
     @buyer = Item.find(params[:item_id])
@@ -7,12 +10,17 @@ class BuyerController < ApplicationController
 
 
 private
- 
-  def  move_to_index
-    unless user_signed_in?
+def  current_user_cant_buy
+    @buyer = Item.find(params[:item_id])
+    if current_user.id == @buyer.user_id
+    redirect_to root_path
+    end
+end
+
+def  move_to_index
+  unless user_signed_in?
     redirect_to new_user_session_path 
   end
-
 end
 
 end
