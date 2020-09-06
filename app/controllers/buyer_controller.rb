1287a4c722726ed5f88db2c2245e4d3e4cfc731a
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-06 17:44:03 +0900

    ログインしていないユーザーは購入ページに遷移しようとすると、ログインページに遷移

--- app/controllers/buyer_controller.rb
+++ app/controllers/buyer_controller.rb
@@ -1,13 +1,18 @@
 class BuyerController < ApplicationController
+  before_action :move_to_index, except:
+
   def index
     @buyer = Item.find(params[:item_id])
-    
-    # binding.pry
-    
-    
   end
 
 
 private
+ 
+  def  move_to_index
+    unless user_signed_in?
+    redirect_to new_user_session_path 
+  end
+
+end
 
 end
