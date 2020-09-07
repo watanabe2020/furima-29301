19af802c20046c4fb7ed3bf4b8393da33ba7c716
20200305 <69565519+20200305@users.noreply.github.com>
2020-09-07 12:10:36 +0900

    小数点の削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -6,7 +6,7 @@ let profit = document.getElementById('profit');
 
 item_price.addEventListener('input', function(){
   const test = document.getElementById('item-price').value;
-  add_tax_price.innerHTML = (test*0.1);
-  profit.innerHTML = (test*0.9);
+  add_tax_price.innerHTML = Math.round(test*0.1);
+  profit.innerHTML = Math.round(test*0.9);
 })
 })
